3346f8abcb1adbad434021c035bbfc3475597245
Necto <necto.ne@gmail.com>
2015-02-17 16:02:51 +0100

    graphical report generation through R::ggplot

--- bench/engine.cpp
+++ bench/engine.cpp
@@ -66,7 +66,7 @@ void dump(map<string, long> results[][3][3])
 
 int main(int argc, char** argv)
 {
-    const int count = 100;
+    const int count = 1000;
 
     int keys[100];
     int busybits[100] = {};
