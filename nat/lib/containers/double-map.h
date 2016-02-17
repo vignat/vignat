a74e9659c75401b7db7936ee2dc3a0b7c9c52d71
Necto <necto.ne@gmail.com>
2016-02-17 22:29:22 +0100

    Adapted the dmap API spec stubs for the new design.

--- nat/lib/containers/double-map.h
+++ nat/lib/containers/double-map.h
@@ -20,19 +20,19 @@ int dmap_allocate(int key_a_size, int key_a_offset,
                   int key_b_size, int key_b_offset,
                   int value_size);
 //@ requires 0 < key_a_size &*& 0 < key_b_size &*& 0 < value_size;
-//@ ensures result == 0 ? true : double_map_p(?map, key_a_size, key_b_size, value_size);
+//@ ensures result == 0 ? true : double_map_p(?map, key_a_size, key_a_offset, key_b_size, key_b_offset, value_size);
 
 int dmap_get_a(void* key, int* index);
-/*@ requires double_map_p(?map, ?key_a_size, ?key_b_size, ?value_size); @*/
-/*@ ensures double_map_p(map, key_a_size, key_b_size, value_size) &*&
+/*@ requires double_map_p(?map, ?key_a_size, ?a_offset, ?key_b_size, ?b_offset, ?value_size); @*/
+/*@ ensures double_map_p(map, key_a_size, a_offset, key_b_size, b_offset, value_size) &*&
             result == 0 ? dmap_has_a(map, key) == false :
                           (dmap_has_a(map, key) == true &*&
                            *index |-> ?i &*& i == domap_get_a(map, key) &*&
                            0 <= i &*& i < DMAP_CAPACITY);
 @*/
 int dmap_get_b(void* key, int* index);
-/*@ requires double_map_p(?map, ?key_a_size, ?key_b_size, ?value_size); @*/
-/*@ ensures double_map_p(map, key_a_size, key_b_size, value_size) &*&
+/*@ requires double_map_p(?map, ?key_a_size, ?a_offset, ?key_b_size, ?b_offset, ?value_size); @*/
+/*@ ensures double_map_p(map, key_a_size, a_offset, key_b_size, b_offset, value_size) &*&
             result == 0 ? dmap_has_b(map, key) == false :
                           (dmap_has_b(map, key) == true &*&
                           *index |-> ?i &*& i == domap_get_b(map, key) &*&
@@ -40,11 +40,11 @@ int dmap_get_b(void* key, int* index);
   @*/
 int dmap_put(void* value, int index);
 int dmap_erase(void* key_a, void* key_b);
-void dmap_get_value(int index, void* valut_out);
-/*@ requires double_map_p(?map, ?key_a_size, ?key_b_size, ?value_size) &*&
+void dmap_get_value(int index, void* value_out);
+/*@ requires double_map_p(?map, ?key_a_size, ?a_offset, ?key_b_size, ?b_offset, ?value_size) &*&
              0 <= index &*& index < DMAP_CAPACITY; @*/
-/*@ ensures double_map_p(map, key_a_size, key_b_size, value_size) &*&
-            result != 0 &*& domap_flow_at(map, index, result); @*/
+/*@ ensures double_map_p(map, key_a_size, a_offset, key_b_size, b_offset, value_size) &*&
+            value_out != 0 &*& domap_flow_at(map, index, value_out); @*/
 void dmap_set_value(int index, void* value);
 //^^^ TODO: add the user-defined predicate here somehow.
 int dmap_size(void);
