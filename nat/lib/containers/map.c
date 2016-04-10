40f042c4aa02f01b7d6a9b4949751c7e403119c2
Necto <necto.ne@gmail.com>
2016-04-10 18:18:46 +0200

    Contracts for the internal function of single map implementation.

--- nat/lib/containers/map.c
+++ nat/lib/containers/map.c
@@ -3,20 +3,72 @@
 
 #include "map.h"
 
-static
-int loop(int k, int capacity)
-{
-    int g = k%capacity;
-    int res = (g + capacity)%capacity;
-    return res;
-}
-
 //TODO: introduce the "chain continuation" bit to optimize search for abscent.
 
+/*@
+  lemma void div_mod(int g, int k, int l)
+  requires g == (k % l) &*& l > 0;
+  ensures (-l <= g) &*& (g < l);
+  {
+  div_rem(k, l);
+  }
+  @*/
+
+/*@
+  lemma void div_mod_gt_0(int mod, int div, int whole)
+  requires mod == (div % whole) &*& whole > 0 &*& div >= 0;
+  ensures (0 <= mod) &*& (mod < whole);
+  {
+  div_rem(div, whole);
+  }
+  @*/
+/*@
+  fixpoint int loop_fp(int k, int capacity)
+  { return ((k%capacity + capacity)%capacity); }
+  @*/
+
 static
-int find_key(int* busybits, void** keyps, int* k_hashes, int start,
-             void* keyp, map_keys_equality* eq, int key_hash,
-             int capacity)
+int loop(int k, int capacity)
+//@ requires true;
+/*@ ensures 0 <= result &*& result < capacity &*&
+            result == loop_fp(k, capacity); @*/
+{
+  int g = k%capacity;
+  //@ div_mod(g, k, CAPACITY);
+  int res = (g + capacity)%capacity;
+  //@ div_mod_gt_0(res, g + CAPACITY, CAPACITY);
+  return res;
+}
+
+/*@
+  inductive hmap<kt> = hmap(list<pair<bool, pair<kt, int> > >);
+
+  predicate hmapping<kt>(hmap<kt> m,
+                         predicate (void*, kt) keyp,
+                         fixpoint (kt, int) hash,
+                         int capacity,
+                         int* busybits,
+                         void** keyps,
+                         int* k_hashes);
+
+  fixpoint bool hmap_exists_key_fp<kt>(hmap<kt> m, void* keyp);
+  fixpoint int hmap_find_key_fp<kt>(hmap<kt> m, void* keyp);
+  fixpoint int hmap_size_fp<kt>(hmap<kt> m);
+  fixpoint int hmap_find_empty_fp<kt>(hmap<kt> m, int start);
+  @*/
+
+static
+int find_key/*@ <kt> @*/(int* busybits, void** keyps, int* k_hashes, int start,
+                         void* keyp, map_keys_equality* eq, int key_hash,
+                         int capacity)
+/*@ requires hmapping<kt>(?hm, ?kp, ?hsh, capacity, busybits, keyps, k_hashes) &*&
+             kp(keyp, ?k) &*&
+             hsh(k) == key_hash; @*/
+/*@ ensures hmapping<kt>(hm, kp, hsh, capacity, busybits, keyps, k_hashes) &*&
+            kp(keyp, k) &*&
+            (hmap_exists_key_fp(hm, keyp) ?
+             (result == hmap_find_key_fp(hm, keyp)) :
+             (result == -1)); @*/
 {
     int i = 0;
     for (; i < capacity; ++i)
@@ -33,7 +85,12 @@ int find_key(int* busybits, void** keyps, int* k_hashes, int start,
 }
 
 static
-int find_empty(int* busybits, int start, int capacity)
+int find_empty/*@ <kt> @*/(int* busybits, int start, int capacity)
+/*@ requires hmapping<kt>(?hm, ?kp, ?hsh, capacity, busybits, ?keyps, ?k_hashes); @*/
+/*@ ensures hmapping<kt>(hm, kp, hsh, capacity, busybits, keyps, k_hashes) &*&
+  (hmap_size_fp(hm) < capacity ?
+   result == hmap_find_empty_fp(hm, start) :
+   result == -1) ; @*/
 {
     int i = 0;
     for (; i < capacity; ++i)
@@ -47,7 +104,7 @@ int find_empty(int* busybits, int start, int capacity)
     return -1;
 }
 
-void map_initialize(int* busybits, map_keys_equality* eq, int capacity) {
+void map_initialize/*@ <kt> @*/(int* busybits, map_keys_equality* eq, int capacity) {
   (void)eq;
   int i = 0;
   for (; i < capacity; ++i) {
@@ -55,9 +112,9 @@ void map_initialize(int* busybits, map_keys_equality* eq, int capacity) {
   }
 }
 
-int map_get(int* busybits, void** keyps, int* k_hashes, int* values,
-            void* keyp, map_keys_equality* eq, int hash, int* value,
-            int capacity)
+int map_get/*@ <kt> @*/(int* busybits, void** keyps, int* k_hashes, int* values,
+                        void* keyp, map_keys_equality* eq, int hash, int* value,
+                        int capacity)
 {
     int start = loop(hash, capacity);
     int index = find_key(busybits, keyps, k_hashes, start,
@@ -70,9 +127,9 @@ int map_get(int* busybits, void** keyps, int* k_hashes, int* values,
     return 1;
 }
 
-int map_put(int* busybits, void** keyps, int* k_hashes, int* values,
-            void* keyp, int hash, int value,
-            int capacity)
+int map_put/*@ <kt> @*/(int* busybits, void** keyps, int* k_hashes, int* values,
+                        void* keyp, int hash, int value,
+                        int capacity)
 {
     int start = loop(hash, capacity);
     int index = find_empty(busybits, start, capacity);
@@ -88,8 +145,8 @@ int map_put(int* busybits, void** keyps, int* k_hashes, int* values,
     return 1;
 }
 
-int map_erase(int* busybits, void** keyps, int* k_hashes, void* keyp,
-              map_keys_equality* eq, int hash, int capacity)
+int map_erase/*@ <kt> @*/(int* busybits, void** keyps, int* k_hashes, void* keyp,
+                          map_keys_equality* eq, int hash, int capacity)
 {
     int start = loop(hash, capacity);
     int index = find_key(busybits, keyps, k_hashes, start,
@@ -102,7 +159,7 @@ int map_erase(int* busybits, void** keyps, int* k_hashes, void* keyp,
     return 1;
 }
 
-int map_size(int* busybits, int capacity)
+int map_size/*@ <kt> @*/(int* busybits, int capacity)
 {
     int s = 0;
     int i = 0;
