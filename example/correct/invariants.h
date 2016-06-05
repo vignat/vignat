e9918937c7b1497b2d38dde1e28500896b11ca2f
Necto <necto.ne@gmail.com>
2016-06-05 17:18:02 +0200

    the set of showcases is now in the separate repository: 'example-vnds'

--- example/correct/invariants.h
+++
@@ -1,9 +0,0 @@
-#include "cell.h"
-
-void invariant_consume(struct cell* cp);
-//@ requires cellp(cp, _);
-//@ ensures true;
-
-void invariant_produce(struct cell** cpp);
-//@ requires *cpp |-> _;
-//@ ensures *cpp |-> ?cp &*& cellp(cp, _);
--- example/correct/net.h
+++
@@ -1,2 +0,0 @@
-int* recv();
-void send(int*);
