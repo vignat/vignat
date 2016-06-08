87e551fda8455cb35bfc2e202a173b581e31ed06
Necto <necto.ne@gmail.com>
2016-06-08 19:25:49 +0200

    Validator refactoring: Function_spec externatiolization continues.

--- validator/validator.ml
+++ validator/validator.ml
@@ -2,13 +2,18 @@ open Core.Std
 open Ir
 
 let validate_prefix fin fout intermediate_pref verifast_bin =
+  let spec_mod =  match !Fspec_api.spec with
+    | Some m -> m
+    | None -> failwith "Failed: could not find function spec dynamic module"
+  in
+  let module Spec = (val spec_mod) in
   let assumptions_fname = intermediate_pref ^ ".assumptions.vf" in
   let lino_fname = intermediate_pref ^ ".lino.int" in
   let export_out_fname = intermediate_pref ^ ".export.stdout" in
   let verify_out_fname = intermediate_pref ^ ".verify.stdout" in
   let ir_fname = intermediate_pref ^ ".ir" in
   let intermediate_fout = intermediate_pref ^ ".tmp.c" in
-  let ir = Import.build_ir Function_spec.fun_types fin in
+  let ir = Import.build_ir Spec.fun_types fin in
   Out_channel.write_all ir_fname ~data:(Sexp.to_string (sexp_of_ir ir));
   Render.render_ir ir intermediate_fout;
   match Verifier.verify_file verifast_bin intermediate_fout verify_out_fname with
@@ -22,7 +27,7 @@ let validate_prefix fin fout intermediate_pref verifast_bin =
           assumptions_fname lino_fname export_out_fname
       in
       let ir = Analysis.induce_symbolic_assignments
-          Function_spec.fixpoints ir vf_assumptions
+          Spec.fixpoints ir vf_assumptions
       in
       Render.render_ir ir fout;
       match Verifier.verify_file verifast_bin fout verify_out_fname with
@@ -31,4 +36,5 @@ let validate_prefix fin fout intermediate_pref verifast_bin =
     end
 
 let () =
+  Fspec_api.spec := Some (module Function_spec) ;
   validate_prefix Sys.argv.(1) Sys.argv.(2) Sys.argv.(3) Sys.argv.(4)
