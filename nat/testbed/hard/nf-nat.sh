a9d306deb44d27b61f0fa32d40aa8208e1ca120a
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2016-09-06 17:57:43 +0200

    NF nat durable script - may be run at any time: after my nat is deployed, after nf nat is deployed

--- nat/testbed/hard/nf-nat.sh
+++ nat/testbed/hard/nf-nat.sh
@@ -1,7 +1,9 @@
 pkill -9 nat
-sudo ~/dpdk/tools/dpdk_nic_bind.py -u 0000:02:00.1
-sudo ~/dpdk/tools/dpdk_nic_bind.py -u 0000:02:00.2
+echo "unbinding em2, em3"
+sudo ~/dpdk/tools/dpdk_nic_bind.py -b igb 0000:02:00.1 0000:02:00.2
 
+sleep 8
+echo "configuring ip addresses"
 ifconfig em2 up
 ip addr add 192.168.2.2/24 dev em2
 ifconfig em3 up
@@ -10,6 +12,11 @@ ip addr add 192.168.3.2/24 dev em3
 sysctl -w net.ipv4.ip_forward=1
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 
+echo "wiping old iptables rules"
+iptables -F FORWARD
+iptables -t nat -F POSTROUTING
+
+echo "installing forwarding rules"
 iptables -t nat -A POSTROUTING -o em2 -j MASQUERADE
 iptables -A FORWARD -i em3 -o em2 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i em3 -o em2 -j ACCEPT
