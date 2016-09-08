6c87a5b66d735eece5c7e9d4388e23ca65bfbad4
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2016-09-08 18:10:51 +0200

    externalize parameters for the NetFilter nat start script

--- nat/testbed/hard/nf-nat.sh
+++ nat/testbed/hard/nf-nat.sh
@@ -1,13 +1,17 @@
+. ./config.sh
+
 pkill -9 nat
-echo "unbinding em2, em3"
-sudo ~/dpdk/tools/dpdk_nic_bind.py -b igb 0000:02:00.1 0000:02:00.2
+echo "unbinding $NAT_PCI_INTERNAL, $NAT_PCI_EXTERNAL"
+sudo ~/dpdk/tools/dpdk-devbind.py -b igb $NAT_PCI_INTERNAL $NAT_PCI_EXTERNAL
 
 sleep 8
 echo "configuring ip addresses"
-ifconfig em2 up
-ip addr add 192.168.2.2/24 dev em2
-ifconfig em3 up
-ip addr add 192.168.3.2/24 dev em3
+ifconfig $NAT_DEVICE_EXTERNAL up
+ip addr flush dev $NAT_DEVICE_EXTERNAL
+ip addr add $NAT_IP_EXTERNAL/24 dev $NAT_DEVICE_EXTERNAL
+ifconfig $NAT_DEVICE_INTERNAL up
+ip addr flush dev $NAT_DEVICE_INTERNAL
+ip addr add $NAT_IP_INTERNAL/24 dev $NAT_DEVICE_INTERNAL
 
 sysctl -w net.ipv4.ip_forward=1
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
@@ -17,6 +21,6 @@ iptables -F FORWARD
 iptables -t nat -F POSTROUTING
 
 echo "installing forwarding rules"
-iptables -t nat -A POSTROUTING -o em2 -j MASQUERADE
-iptables -A FORWARD -i em3 -o em2 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i em3 -o em2 -j ACCEPT
+iptables -t nat -A POSTROUTING -o $NAT_DEVICE_EXTERNAL -j MASQUERADE
+iptables -A FORWARD -i $NAT_DEVICE_INTERNAL -o $NAT_DEVICE_EXTERNAL -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i $NAT_DEVICE_INTERNAL -o $NAT_DEVICE_EXTERNAL -j ACCEPT
