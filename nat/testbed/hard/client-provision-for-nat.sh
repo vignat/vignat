3bab959901b3e4710b4c7ed14bcb8ed632b78d3e
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2016-09-08 14:22:42 +0200

    flush ips before provisioning on client/server

--- nat/testbed/hard/client-provision-for-nat.sh
+++ nat/testbed/hard/client-provision-for-nat.sh
@@ -1,7 +1,9 @@
 NAT_INTERNAL_MAC=00:1e:67:92:29:6d
 ifconfig em2 up
+ip addr flush dev em2
 ip addr add 192.168.0.5/24 dev em2
 ifconfig em3 up
+ip addr flush dev em3
 ip addr add 192.168.3.5/24 dev em3
 ip route add 192.168.2.0/24 via 192.168.3.2 dev em3
 arp -s 192.168.3.2 $NAT_INTERNAL_MAC
--- nat/testbed/hard/server-provision-for-nat.sh
+++ nat/testbed/hard/server-provision-for-nat.sh
@@ -1,7 +1,9 @@
 NAT_EXTERNAL_MAC=00:1e:67:92:29:6c
 ifconfig em2 up
+ip addr flush dev em2
 ip addr add 192.168.2.10/24 dev em2
 ifconfig em3 up
+ip addr flush dev em3
 ip addr add 192.168.0.10/24 dev em3
 arp -s 192.168.2.2 $NAT_EXTERNAL_MAC
 #hping3 192.168.33.10 -k -p 47882 -s 0
