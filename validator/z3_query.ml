198d9ef49b975840d54e72f3828dbae04e9c7d80
Necto <necto.ne@gmail.com>
2016-09-10 22:36:32 +0200

    added support for structure fields for Z3 query generator

--- validator/z3_query.ml
+++ validator/z3_query.ml
@@ -60,23 +60,25 @@ let show_result result solver =
 let register_symbs_z3 sttmts ctx ints =
   let var_map = ref String.Map.empty in
   let fun_map = ref String.Map.empty in
+  let add_fun fname args =
+    match String.Map.find !fun_map fname with
+    | Some _ -> ()
+    | None ->
+      let domain = List.map args ~f:(fun _ -> ints) in
+      fun_map := String.Map.add !fun_map ~key:fname
+          ~data:(mk_fresh_func_decl ctx fname domain ints)
+  in
   List.iter sttmts ~f:(fun sttmt ->
       ignore (Ir.call_recursively_on_tterm (function
       | Ir.Id x -> begin match String.Map.find !var_map x with
-          | Some _ -> None
+          | Some _ -> ()
           | None -> var_map := String.Map.add !var_map ~key:x
                 ~data:(Expr.mk_const ctx
-                         (mk_string ctx x) ints);
-            None
-        end
-      | Apply (fname,args) -> begin match String.Map.find !fun_map fname with
-          | Some _ -> None
-          | None ->
-            let domain = List.map args ~f:(fun _ -> ints) in
-            fun_map := String.Map.add !fun_map ~key:fname
-                ~data:(mk_fresh_func_decl ctx fname domain ints);
-            None
-        end
+                         (mk_string ctx x) ints)
+        end;
+        None
+      | Apply (fname,args) -> add_fun fname args; None
+      | Str_idx (arg,fname) -> add_fun fname [arg]; None
       | _ -> None) sttmt));
   (!var_map,!fun_map)
 
@@ -101,28 +103,41 @@ let tterm_to_z3 tterm ctx var_map fun_map ints =
       let f = String.Map.find_exn fun_map fname in
       Expr.mk_app ctx f (List.map args ~f:run)
   | Ir.Id x -> String.Map.find_exn var_map x
-  | Ir.Struct (name,fds) -> failwith "no structures for a moment"
+  | Ir.Struct (_,_) -> failwith ("no structures for a moment: " ^
+                                 (Ir.render_tterm tterm))
   | Ir.Int i -> Expr.mk_numeral_int ctx i ints
   | Ir.Bool b -> Expr.mk_numeral_int ctx (if b then 1 else 0) ints
   | Ir.Not x -> Boolean.mk_not ctx (run x)
-  | Ir.Str_idx (tt,fname) -> failwith "no structure destructure for now"
-  | Ir.Deref tt -> failwith "no support for dereferences"
-  | Ir.Fptr fname -> failwith "no support for fptrs"
-  | Ir.Addr tt -> failwith "no spport for addrtaking"
-  | Ir.Cast (ctype,tt) -> run tt
+  | Ir.Str_idx (x,fname) -> run {Ir.t=tterm.Ir.t;
+                                 Ir.v=Ir.Apply (fname,[x])}
+  | Ir.Deref _ -> failwith "no support for dereferences"
+  | Ir.Fptr _ -> failwith "no support for fptrs"
+  | Ir.Addr _ -> failwith "no spport for addrtaking"
+  | Ir.Cast (_,tt) -> run tt
   | Ir.Undef -> failwith "what should I do with undef?"
   in
   run tterm
 
-let statement_to_z3 sttmt ctx funs vars ints =
+let struct_eq_to_z3 ctx (fields : Ir.var_spec list) term funs vars ints =
+  let subterms = List.map fields ~f:(fun {name;value} ->
+      let f = String.Map.find_exn funs name in
+      Boolean.mk_eq ctx (Expr.mk_app ctx f [term]) (tterm_to_z3 value ctx vars funs ints))
+  in
+  Boolean.mk_and ctx subterms
+
+let statement_to_z3 sttmt ctx vars funs ints =
   match sttmt.Ir.v with
-  | Ir.Bop _ -> tterm_to_z3 sttmt ctx funs vars ints
-  | Ir.Not _ -> tterm_to_z3 sttmt ctx funs vars ints
+  | Ir.Bop (Ir.Eq,{t=_;v=Ir.Struct (_,fields)},x)
+  | Ir.Bop (Ir.Eq,x,{t=_;v=Ir.Struct (_,fields)}) ->
+    failwith ("no support for structural equality: " ^ (Ir.render_tterm sttmt))
+    (* struct_eq_to_z3 ctx fields (tterm_to_z3 x ctx vars funs ints) funs vars ints *)
+  | Ir.Bop _ -> tterm_to_z3 sttmt ctx vars funs ints
+  | Ir.Not _ -> tterm_to_z3 sttmt ctx vars funs ints
   | Ir.Apply _ -> tterm_to_z3 {Ir.t=Unknown;
                                Ir.v=Ir.Bop (Ir.Eq,
                                             {Ir.t=Unknown;Ir.v=Ir.Int 1},
                                             sttmt)}
-                    ctx funs vars ints
+                    ctx vars funs ints
   | Ir.Bool true -> Boolean.mk_true ctx
   | _ -> failwith ("incorrect statement " ^ (Ir.render_tterm sttmt))
 
