14870eb09c0390d24b30ddc3171e8c586730a666
Necto <necto.ne@gmail.com>
2016-09-14 00:14:51 +0200

    Fixes for the commision scripts.

--- nat/testbed/hard/client-commission-pktgen.sh
+++ nat/testbed/hard/client-commission-pktgen.sh
@@ -1,11 +1,11 @@
 sudo apt-get update
 KERNEL_RELEASE=$(uname -r)
-sudo apt-get install -y tcpdump hping3 python-scapy git wget build-essential libpcap-dev linux-headers-$KERNEL_RELEASE libglib2.0-dev daemon iperf3 netperf liblua5.3-dev
+sudo apt-get install -y tcpdump hping3 python-scapy git wget build-essential libpcap-dev linux-headers-$KERNEL_RELEASE libglib2.0-dev daemon iperf3 netperf liblua5.3-dev make binutils gcc
 
 wget http://fast.dpdk.org/rel/dpdk-16.07.tar.xz -O dpdk.tar.xz
 tar xf dpdk.tar.xz
 mv dpdk-* dpdk
-rm dpdk.tar.gz
+rm dpdk.tar.xz
 
 cd dpdk
 sed -ri 's,(PMD_PCAP=).*,\1y,' config/common_linuxapp
@@ -17,7 +17,7 @@ export RTE_TARGET=x86_64-native-linuxapp-gcc
 
 cd ~
 wget http://dpdk.org/browse/apps/pktgen-dpdk/snapshot/pktgen-3.0.13.tar.gz -O pktgen.tar.gz
-tar xf pktgen
+tar xf pktgen.tar.gz
 mv pktgen-* pktgen
 rm pktgen.tar.gz
 
--- nat/testbed/hard/nat-commission.sh
+++ nat/testbed/hard/nat-commission.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
-sudo apt-get install -y tcpdump git wget build-essential libpcap-dev linux-headers-3.13.0-93 linux-headers-3.13.0-93-generic libglib2.0-dev daemon iperf3 netperf
+KERNEL_RELEASE=$(uname -r)
+sudo apt-get install -y tcpdump git wget build-essential libpcap-dev linux-headers-3.13.0-93 linux-headers-$KERNEL_RELEASE libglib2.0-dev daemon iperf3 netperf
 
 wget http://fast.dpdk.org/rel/dpdk-16.07.tar.xz -O dpdk.tar.xz
 tar xf dpdk.tar.xz
