0986da9d9f726b94dccc6829d49e85791f10d4e0
Necto <necto.ne@gmail.com>
2016-09-14 09:22:49 +0200

    pktgen-commission for the sernder/receiver machines.

--- nat/testbed/hard/pktgen-commission.sh
+++ nat/testbed/hard/pktgen-commission.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 KERNEL_RELEASE=$(uname -r)
-sudo apt-get install -y tcpdump hping3 python-scapy git wget build-essential libpcap-dev linux-headers-$KERNEL_RELEASE libglib2.0-dev daemon iperf3 netperf liblua5.3-dev make binutils gcc
+sudo apt-get install -y tcpdump hping3 python-scapy git wget build-essential libpcap-dev linux-headers-$KERNEL_RELEASE libglib2.0-dev daemon iperf3 netperf liblua5.2-dev make binutils gcc
 
 wget http://fast.dpdk.org/rel/dpdk-16.07.tar.xz -O dpdk.tar.xz
 tar xf dpdk.tar.xz
