cdda26c26b75292e50b074d7155bb2043c2b897a
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2016-09-14 22:16:21 +0200

    cpu frequency control scripts

---
+++ nat/testbed/hard/cpu_slow.sh
@@ -0,0 +1,2 @@
+for i in $(seq 1 31); do echo userspace > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor; done
+for i in $(seq 1 31); do echo 1200000 > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_setspeed; done
