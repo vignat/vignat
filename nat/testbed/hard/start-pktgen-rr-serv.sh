cc36c96208bad4f02583598b040822beeadd4a37
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2016-09-18 21:11:54 +0200

    This bash magic just kills me

--- nat/testbed/hard/start-pktgen-rr-serv.sh
+++ nat/testbed/hard/start-pktgen-rr-serv.sh
@@ -1,3 +1,1 @@
-cd ~/pktgen
-sleep 9999999999 | ./app/app//x86_64-native-linuxapp-gcc/pktgen -c 0x1f -n 3 -- -P -m "[1-2:3-4].0" -f ../scripts/pktgen-scripts/provision-rr.lua -G &> ./pktgen-log.txt
-disown %1
+cd ~/pktgen && fifo=$(mktemp -u) && mkfifo "$fifo" && (rm "$fifo" && { sudo ./app/app//x86_64-native-linuxapp-gcc/pktgen -c 0x1f -n 3 -- -P -m "[1-2:3-4].0" -f ../scripts/pktgen-scripts/provision-rr.lua -G < /dev/fd/1 3&>1 > ./pktgen-log.txt <&3 3<&-; } &) 3<> "$fifo" | :
