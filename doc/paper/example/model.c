2d7e3a256afdf553747be6ea5324839c6328f846
Necto <necto.ne@gmail.com>
2016-10-20 13:33:45 +0200

    paper example symbolic model expanded for the new ring API and proper call tracing.

--- doc/paper/example/model.c
+++ doc/paper/example/model.c
@@ -2,15 +2,17 @@
 #include "packet.h"
 #include "ring.h"
 
-int main() {
+int main(int argc, char** argv) {
   struct packet p;
+  struct ring *r = ring_create();
+  //mistake: no check for null here!
   while(LOOP(1))
   {// Loop iteration begins.
-    if (!ring_full())
+    if (!ring_full(r))
       if (receive_packet(&p) && p.port != 9)
-        ring_push_back(&p);
-    if (!ring_empty() && can_send_packet()) {
-      ring_pop_front(&p);
+        ring_push_back(r, &p);
+    if (!ring_empty(r) && can_send_packet()) {
+      ring_pop_front(r, &p);
       ASSERT(p.port != 9);
       send_packet(&p);
     }
--- doc/paper/example/ring-stub.c
+++ doc/paper/example/ring-stub.c
@@ -1,15 +1,47 @@
 #include "vigor.h"
 #include "ring.h"
 
-bool ring_full() { return SYMBOLIC("full"); }
+struct ring {
+  int dummy;
+};
 
-bool ring_empty() { return SYMBOLIC("empty"); }
+struct ring preallocated;
 
-void ring_push_back(struct packet* p) {
+struct ring* ring_create() {
+  klee_trace_ret_just_ptr(sizeof(struct ring));
+  if (SYMBOLIC("alloc_success"))
+    return &preallocated;
+  else
+    return 0;
+}
+
+bool ring_full(struct ring* r) {
+  klee_trace_ret();
+  klee_trace_param_just_ptr(r, sizeof(struct ring), "r");
+  return SYMBOLIC("full");
+}
+
+bool ring_empty(struct ring* r) {
+  klee_trace_ret();
+  klee_trace_param_just_ptr(r, sizeof(struct ring), "r");
+  return SYMBOLIC("empty");
+}
+
+void ring_push_back(struct ring* r, struct packet* p) {
+  klee_trace_ret();
+  klee_trace_param_just_ptr(r, sizeof(struct ring), "r");
+  klee_trace_param_ptr(p, sizeof(struct packet), "p");
+  klee_trace_param_ptr_field(p, offsetof(struct packet, port),
+                             sizeof(int), "port");
   ASSERT(p->port != 9); //Unnecessary.
 }
 
-void ring_pop_front(struct packet* p) {
+void ring_pop_front(struct ring* r, struct packet* p) {
+  klee_trace_ret();
+  klee_trace_param_just_ptr(r, sizeof(struct ring), "r");
+  klee_trace_param_ptr(p, sizeof(struct packet), "p");
+  klee_trace_param_ptr_field(p, offsetof(struct packet, port),
+                             sizeof(int), "port");
   FILL_SYMBOLIC(p, sizeof(struct packet), "popped_packet");
   ASSUME(p->port != 9);
 }
