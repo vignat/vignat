f53263240c4558c537b25d098fbc3f69f23ac285
Necto <necto.ne@gmail.com>
2016-10-20 15:05:31 +0200

    Basic make rules for validation.

--- doc/paper/example/loop-model.c
+++ doc/paper/example/loop-model.c
@@ -12,10 +12,10 @@ void loop_iteration_end(struct ring **r) {
 
 void loop_invariant_consume(struct ring **r) {
   klee_trace_ret();
-  klee_trace_param_just_ptr(r, sizeof(struct ring*), "r");
+  klee_trace_param_ptr(r, sizeof(struct ring*), "r");
 }
 
 void loop_invariant_produce(struct ring **r) {
   klee_trace_ret();
-  klee_trace_param_just_ptr(r, sizeof(struct ring*), "r");
+  klee_trace_param_ptr(r, sizeof(struct ring*), "r");
 }
