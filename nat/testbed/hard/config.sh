66fe03177928d863af2bedd433f2d99d515b88f3
Solal Pirelli <solal.pirelli@gmail.com>
2017-01-02 18:43:04 +0100

    [testbed/hard] Minor fixes to config file.

--- nat/testbed/hard/config.sh
+++ nat/testbed/hard/config.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
-# The main configuration file of the network for performance testing.
-# It binds the real hosts, addresses and ports to the abstract names used
-# throughout the scripts.
 
-# the network:
-# tester{icnalsp3s1}[em3] 192.168.3.5 -- 192.168.3.2 [em3]mdbox{icnalsp3s2}
-# tester{icnalsp3s1}[em2] 192.168.2.10 -- 192.168.2.2 [em2]mdbox{icnalsp3s2}
-# mdbox{icnalsp3s2}[em4] 192.168.200.2 -- 192.168.200.10 [em4]server{icnalsp3s3}
+# Configuration file mapping real hosts, addresses and ports
+# to abstract names used in the scripts.
 
-TESTER_HOST=icnalsp3s1.epfl.ch
+# Subnets
+
+EXTERNAL_SUBNET=192.168.2.0
+SERVER_SUBNET=192.168.200.0
+
+# Middlebox
+
 MB_HOST=icnalsp3s2.epfl.ch
-SERVER_HOST=icnalsp3s3.epfl.ch
 
 MB_INTERNAL_MAC=00:1e:67:92:29:6d
 MB_EXTERNAL_MAC=00:1e:67:92:29:6c
 MB_TO_SRV_MAC=00:1e:67:92:29:6e
 
 MB_IP_INTERNAL=192.168.3.2
-EXTERNAL_SUBNET=192.168.2.0
-SERVER_SUBNET=192.168.200.0
 MB_IP_EXTERNAL=192.168.2.2
 MB_IP_TO_SRV=192.168.200.2
 
@@ -29,6 +27,10 @@ MB_DEVICE_TO_SRV=em4
 MB_PCI_INTERNAL='0000:02:00.2'
 MB_PCI_EXTERNAL='0000:02:00.1'
 MB_PCI_TO_SRV='0000:02:00.3'
+
+# Tester
+
+TESTER_HOST=icnalsp3s1.epfl.ch
 
 TESTER_DEVICE_INTERNAL=em3
 TESTER_DEVICE_EXTERNAL=em2
@@ -43,12 +45,15 @@ TESTER_IP_INTERNAL=192.168.3.5
 TESTER_IP_EXTERNAL=192.168.2.10
 TESTER_IP_FOR_STUB=192.168.200.5
 
+# Server
+
+SERVER_HOST=icnalsp3s3.epfl.ch
+
 SERVER_IP=192.168.200.10
 SERVER_MAC=00:1e:67:92:2a:2b
 SERVER_DEVICE=em4
 
-export RTE_SDK=$HOME/dpdk
-export RTE_TARGET=x86_64-native-linuxapp-gcc
+# Other
 
 NAT_SRC_PATH=$HOME/vnds/nat
 STUB_SRC_PATH=$HOME/vnb-nat
