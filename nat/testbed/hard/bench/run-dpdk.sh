ed507df5326a07ba189751fb5e0b1502067c51e0
Solal Pirelli <solal.pirelli@gmail.com>
2017-01-20 16:34:55 +0100

    Small fix.

--- nat/testbed/hard/bench/run-dpdk.sh
+++ nat/testbed/hard/bench/run-dpdk.sh
@@ -32,14 +32,12 @@ if [ $1 = "loopback" ]; then
     sudo ./build/nat -c 0x01 -n 2 -- -p 0x3 --wan 0 --lan-dev 1 \
         $3 \
         --extip $MB_IP_EXTERNAL \
-        --eth-dest 0,$TESTER_MAC_EXTERNAL --eth-dest 1,$TESTER_MAC_INTERNAL \
-        --eth-own 0,$MB_MAC_EXTERNAL --eth-own 1,$MB_MAC_INTERNAL
+        --eth-dest 0,$TESTER_MAC_EXTERNAL --eth-dest 1,$TESTER_MAC_INTERNAL
 else
     sudo ./build/nat -c 0x01 -n 2 -- -p 0x7 --wan 2 --lan-dev 1 \
         $3 \
         --extip $MB_IP_TO_SRV \
-        --eth-dest 0,$TESTER_MAC_EXTERNAL --eth-dest 1,$TESTER_MAC_INTERNAL --eth-dest 2,$SERVER_MAC \
-        --eth-own 0,$MB_MAC_EXTERNAL --eth-own 1,$MB_MAC_INTERNAL --eth-own 2,$MB_MAC_TO_SRV
+        --eth-dest 0,$TESTER_MAC_EXTERNAL --eth-dest 1,$TESTER_MAC_INTERNAL --eth-dest 2,$SERVER_MAC
 fi
 
-#popd >> /dev/null
+popd >> /dev/null
