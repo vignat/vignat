55af27057b7218dc6568ce137bd0e041e6562144
Solal Pirelli <solal.pirelli@gmail.com>
2017-07-12 10:44:21 +0200

    dmz builds again (but doesn't work)

--- nf/dmz/dmz_main.c
+++ nf/dmz/dmz_main.c
@@ -8,30 +8,40 @@
 #include <rte_ethdev.h>
 #include <rte_mbuf.h>
 
+#include "../lib/flowmanager.h"
 #include "../lib/nf_forward.h"
 #include "../lib/nf_util.h"
-
-#include "../vignat/flowmanager.h"
 
 #include "dmz_config.h"
 
 struct dmz_config config;
+struct FlowManager* internet_manager;
+struct FlowManager* dmz_manager;
 
 void nf_core_init(void)
 {
-        if (!allocate_flowmanager(rte_eth_dev_count(),
-	                          0, // start port
-                                  0, // external addr
-                                  0, // WAN device
-                                  config.expiration_time,
-                                  config.max_flows)) {
-                rte_exit(EXIT_FAILURE, "Could not allocate flow manager");
+        internet_manager = allocate_flowmanager(rte_eth_dev_count(),
+	                                        0, // start port
+                                                0, // external addr
+                                                0, // WAN device
+                                                config.expiration_time,
+                                                config.max_flows);
+
+        dmz_manager = allocate_flowmanager(rte_eth_dev_count(),
+	                                   0, // start port
+                                           0, // external addr
+                                           0, // WAN device
+                                           config.expiration_time,
+                                           config.max_flows);
+
+        if (internet_manager == NULL || dmz_manager == NULL) {
+                rte_exit(EXIT_FAILURE, "Could not allocate flow managers");
         }
 }
 
-uint8_t nf_core_process(uint8_t device,
-                        struct rte_mbuf* mbuf,
-                        uint32_t now)
+int nf_core_process(uint8_t device,
+                    struct rte_mbuf* mbuf,
+                    uint32_t now)
 {
 	return 0;
 }
@@ -47,3 +57,17 @@ void nf_config_cmdline_print_usage(void) {
 void nf_print_config() {
   dmz_print_config(&config);
 }
+
+#ifdef KLEE_VERIFICATION
+void nf_loop_iteration_begin(unsigned lcore_id,
+                             uint32_t time) {
+}
+
+void nf_add_loop_iteration_assumptions(unsigned lcore_id,
+                                       uint32_t time) {
+}
+
+void nf_loop_iteration_end(unsigned lcore_id,
+                           uint32_t time) {
+}
+#endif //KLEE_VERIFICATION
