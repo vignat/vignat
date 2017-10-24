224d21613968cc3013e18789075498afd0ffb8e2
Solal Pirelli <solal.pirelli@gmail.com>
2017-10-24 14:22:26 +0200

    nf_log doesn't need disabling during symbex

--- nf/lib/nf_log.h
+++ nf/lib/nf_log.h
@@ -1,16 +1,11 @@
 #pragma once
 
-#if KLEE_VERIFICATION
-	#define NF_INFO(...)
-	#define NF_DEBUG(...)
+#include <stdio.h>
+
+#define NF_INFO(text, ...) printf(text "\n", ##__VA_ARGS__); fflush(stdout);
+
+#if ENABLE_LOG
+	#define NF_DEBUG(text, ...) printf("DEBUG: " text "\n", ##__VA_ARGS__); fflush(stdout);
 #else
-	#include <stdio.h>
-
-	#define NF_INFO(text, ...) printf(text "\n", ##__VA_ARGS__); fflush(stdout);
-
-	#if ENABLE_LOG
-		#define NF_DEBUG(text, ...) printf("DEBUG: " text "\n", ##__VA_ARGS__); fflush(stdout);
-	#else
-		#define NF_DEBUG(...)
-	#endif
+	#define NF_DEBUG(...)
 #endif
