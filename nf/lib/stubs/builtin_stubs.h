b2d1b34bc150f7f4d933b91080973aef913d592d
Solal Pirelli <solal.pirelli@gmail.com>
2017-10-30 17:44:58 +0100

    Runs until VigNAT code, but there are 0 devices

--- nf/lib/stubs/builtin_stubs.h
+++ nf/lib/stubs/builtin_stubs.h
@@ -3,3 +3,6 @@
 #define __builtin_constant_p(x) 1
 
 #define __sync_bool_compare_and_swap(ptr, oldval, newval) ((*ptr == oldval) ? (*ptr = newval, 1) : 0)
+
+// DPDK only uses it as an atomic add, no fetch necessary
+#define __sync_fetch_and_add(ptr, value) (*(ptr) += (value))
