2bc1ac5319a8004e42657ca2abb630dfc25be06f
Dragos Tiselice <dragostiselice@gmail.com>
2017-11-17 15:51:10 +0100

    Added simple NAT testing scenarios.

--- nf/testbed/server.sh
+++ nf/testbed/server.sh
@@ -1,3 +1,3 @@
-sudo apt-get update && sudo apt-get install -y tcpdump hping3 apache2
+sudo apt-get update && sudo apt-get install -y tcpdump hping3 apache2 python-scapy
 sudo arp -s 192.168.33.2 08:00:27:00:44:71
 
