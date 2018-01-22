8fe3c44001d405ab7bafd8f372221e9d01897c85
Solal Pirelli <solal.pirelli@gmail.com>
2018-01-22 10:36:46 +0100

    fix memmove problem

--- nf/lib/stubs/external_stubs.c
+++ nf/lib/stubs/external_stubs.c
@@ -285,3 +285,27 @@ getpagesize(void)
 	//return klee_int("page_size"); // TODO - but it propagates a symbol in annoying places
 	return 4096;
 }
+
+// FIXME LLVM uses intrinsics for memmove so we can't use the uclibc one for some reason
+//       (i.e its declaration is not linked in with the rest like e.g. strcmp)
+//       so for now we just copy/paste it from uclibc...
+void*
+memmove(void* s1, const void* s2, size_t n)
+{
+        char* s = (char*) s1;
+        const char* p = (const char*) s2;
+
+        if (p >= s) {
+                while (n) {
+                        *s++ = *p++;
+                        --n;
+                }
+        } else {
+                while (n) {
+                        --n;
+                        s[n] = p[n];
+                }
+        }
+
+        return s1;
+}
