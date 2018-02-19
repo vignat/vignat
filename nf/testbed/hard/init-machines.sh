ab8de8519c8ced0940a8972d3e2f0691a67a4e76
Solal Pirelli <solal.pirelli@gmail.com>
2018-02-19 21:41:48 +0100

    Also put install dir on other machines for install.sh

--- nf/testbed/hard/init-machines.sh
+++ nf/testbed/hard/init-machines.sh
@@ -3,10 +3,12 @@
 
 echo "[init] Cloning scripts..."
 cp ../../../install.sh . # also include the install script for DPDK
+cp -r ../../../install . # and the patch directory it uses
 for h in $TESTER_HOST; do
     rsync -q -t -r --exclude '*.log' --exclude '*.results' ./ $h:scripts
 done
 rm install.sh
+rm -rf install
 
 echo "[init] Initializing all machines..."
 ssh $TESTER_HOST 'bash ~/scripts/init-machines/tester.sh'
